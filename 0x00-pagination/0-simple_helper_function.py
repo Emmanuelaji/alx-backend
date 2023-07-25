f4d9197231faafc4c43b8add901f3b323159643c
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2023-07-25 02:17:03 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,6 @@ Contains definition of index_range helper function
 """
 from typing import Tuple
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Takes 2 integer arguments and returns a tuple of size two
